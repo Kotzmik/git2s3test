ea1623b84244ff94a91d51de88e05b19f7003a06
Kotzmik <elvisio.san@gmail.com>
2020-05-01 18:57:17 +0200

    /test

--- js/test.js
+++ js/test.js
@@ -17,7 +17,7 @@ var WildRydes = window.WildRydes || {};
     function requestUnicorn(file) {
         $.ajax({
             method: 'POST',
-            url: _config.api.invokeUrl + '/ride',
+            url: _config.api.invokeUrl + '/test',
             headers: {
                 Authorization: authToken
             },
