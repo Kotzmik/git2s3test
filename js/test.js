61252ec66cfddae34546804e753125552bd9654f
KotzmikPc <47783794+Kotzmik@users.noreply.github.com>
2020-07-27 15:47:02 +0200

    Update test.js

--- js/test.js
+++ js/test.js
@@ -211,12 +211,5 @@ var WildRydes = window.WildRydes || {};
 	function testalert() {
 		console.log("test");
 	}
-	var modal = document.getElementById('modal');
-
-// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function(event) {
-	if (event.target == modal) {
-		modal.style.display = "none";
-	}
 }
 }(jQuery));
